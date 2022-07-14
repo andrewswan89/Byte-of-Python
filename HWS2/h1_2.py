15eb4be9e5240225d1215f508a25c40c99a90d14
SWAN <andrewswan89@gmail.com>
2022-07-14 23:59:31 +0700

    add task1 s 2

---
+++ HWS2/h1_2.py
@@ -0,0 +1,8 @@
+number = float(input("Введите вещественное число: "))
+
+lst = list(str(number).split('.'))
+summ = 0
+for i in lst:
+    for j in i:
+        summ += int(j)
+print(f"Сумма цифр вещественного числа равна = {summ}").
