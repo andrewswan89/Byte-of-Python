e74f6009f8a1c475db9dd104215892cd7d37cca8
SWAN <andrewswan89@gmail.com>
2022-07-17 18:14:37 +0700

    add one more and one more

--- HWS2/h1_2.py
+++ HWS2/h1_2.py
@@ -14,3 +14,13 @@ for i in num:
         continue
     sum += int(i)
 print(sum)
+
+  #решение со списком и функцией split
+
+#number = float(input("Введите вещественное число: "))
+#lst = list(str(number).split('.'))
+#summ = 0
+#for i in lst:
+ #   for j in i:
+  #      summ += int(j)
+#print(f"Сумма цифр вещественного числа равна = {summ}")
