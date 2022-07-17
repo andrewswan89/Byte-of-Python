61e823746b83156c68067d9b6ead9a25bbb2956a
SWAN <andrewswan89@gmail.com>
2022-07-17 18:11:28 +0700

    add one more

--- HWS2/h1_2.py
+++ HWS2/h1_2.py
@@ -1,6 +1,16 @@
-num = int(input("Число--> "))
+# неверное решетие работает только с целыми
+# num = int(input("Число--> "))
+#sum = 0
+#while (num != 0):
+ #   sum = sum + num % 10
+#  num = num // 10
+# print("Сумма =  ", sum)
+
+# 
+num = input('Введите число: ')
 sum = 0
-while (num != 0):
-    sum = sum + num % 10
-    num = num // 10
-print("Сумма =  ", sum)
+for i in num:
+    if i == '.':
+        continue
+    sum += int(i)
+print(sum)
